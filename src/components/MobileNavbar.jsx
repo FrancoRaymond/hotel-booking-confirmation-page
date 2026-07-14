7acfe5c9f98c8974c06828a08997d91999ecfd73
FrancoRaymond <francoreason@gmail.com>
2026-07-14 13:04:44 +0200

    added fonts and completed mobile nav functionality

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -12,38 +12,41 @@ const MobileNavbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
 
   return (
-    <div className='flex items-center justify-between'>
+    <div className='relative flex items-center justify-between bg-[#fbf6f0] py-2 md:hidden'>
         <img src={logo} alt="maison soleil logo" />
-        <nav className={menuOpen ? '' : 'hidden'}>
+        <nav className={`absolute border-t py-3 border-gray-300 top-14 left-0 w-full shadow-lg overflow-hidden transition-all duration-300 ease-in-out origin-top ${menuOpen ? 'h-screen opacity-100 translate-y-0' : 'max-h-0 opacity-0 -translate-y-3 pointer-events-none'}`}>
             <ul>
                 <li>
-                    <a href="" className='flex items-center gap-2 text-sm p-2 rounded-sm'>
-                        <img src={yourstay} alt="" />
-                        <p >Your stay</p>
+                    <a href="" className='flex bg-white items-center justify-between gap-2 text-sm py-2.5 px-4 rounded-lg'>
+                        <div className='flex items-center gap-2'>
+                            <img src={yourstay} alt="" />
+                            <p >Your stay</p>
+                        </div>
+                        <p className='text-[8px] p-0.5 size-4 flex items-center justify-center rounded-full bg-[#ba3e19] text-white'>1</p>
                     </a>
                 </li>
                 <li>
-                    <a href="" className='flex items-center gap-2 text-sm p-2 rounded-sm'>
+                    <a href="" className='flex items-center gap-2 text-sm py-2.5 px-4 rounded-sm'>
                         <img src={house} alt="" />
-                        <p >The house</p>
+                        <p className='text-gray-600'>The house</p>
                     </a>
                 </li>
                 <li>
-                    <a href="" className='flex items-center gap-2 text-sm p-2 rounded-sm'>
+                    <a href="" className='flex items-center gap-2 text-sm py-2.5 px-4 rounded-sm'>
                         <img src={pin} alt="" />
-                        <p >Around town</p>
+                        <p className='text-gray-600'>Around town</p>
                     </a>
                 </li>
                 <li>
-                    <a href="" className='flex items-center gap-2 text-sm p-2 rounded-sm'>
+                    <a href="" className='flex items-center gap-2 text-sm py-2.5 px-4 rounded-sm'>
                         <img src={breakfast} alt="" />
-                        <p >Breakfast</p>
+                        <p className='text-gray-600'>Breakfast</p>
                     </a>
                 </li>
                 <li>
-                    <a href="" className='flex items-center gap-2 text-sm p-2 rounded-sm'>
+                    <a href="" className='flex items-center gap-2 text-sm py-2.5 px-4 rounded-sm'>
                         <img src={messages} alt="" />
-                        <p >Messages</p>
+                        <p className='text-gray-600'>Messages</p>
                     </a>
                 </li>
             </ul>
